46b0f491687b286f42a698284aa7a5aa24c59ebd
bluer222 <samuelzhimerlin@gmail.com>
2024-11-13 15:31:38 -0500

    switch to using tabs instead of windows

--- README.md
+++ README.md
@@ -10,10 +10,10 @@
 Click the extension icon and input a list of paper names(or some other identifying information that will identify specifically the paper you want) seprated with commas
 
 ### when you press the search button it will do the following:
-1. find these papers with google scholar in a new window
-2. open the first result in a new window and close the search window
+1. find these papers with google scholar in a new tab
+2. open the first result in a new tab and close the search tab
 3. identify a pdf download
     <ol type="a">
-    <li>If identified the pdf will be downloaded and the window will be closed</li>
-    <li>If no dowload is identified then the window will stay open so a human can find the download option</li>
+    <li>If identified the pdf will be downloaded and the tab will be closed</li>
+    <li>If no dowload is identified then the tab will stay open so a human can find the download option</li>
     </ol>
--- background.js
+++ background.js
@@ -13,12 +13,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const searchUrl = 'https://scholar.google.com/scholar?q=';
 
         request.query.forEach(search => {
-            chrome.windows.create({
+            chrome.tabs.create({
                 url: searchUrl + encodeURIComponent(search),
-                type: 'popup',
-                focused: true
-            }, (window) => {
-                searchTabId.push(window.tabs[0].id);
+            }, (tab) => {
+                searchTabId.push(tab.id);
             });
         });
     }
@@ -27,32 +25,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         searchTabId.splice(searchTabId.indexOf(sender.tab.id), 1);
 
         console.log("opening interact url");
-        chrome.windows.create({
+        chrome.tabs.create({
             url: request.tabUrl,
-            type: 'popup',
-            focused: true
-        }, (window) => {
-            interactTabId.push(window.tabs[0].id);
+        }, (tab) => {
+            interactTabId.push(tab.id);
         });
     }
-    if (request.action === 'openDownloadUrl') {
-        //remove item from interact tabs
-        //remove item from search tabs
-        interactTabId.splice(interactTabIdv.indexOf(sender.tab.id), 1);
-
-        console.log("opening download url");
-        chrome.windows.create({
-            url: request.tabUrl,
-            type: 'popup',
-            focused: true
-        }, (window) => {
-            // interactTabId = window.tabs[0].id;
-        });
-    }
-    /*
-if (request.action === 'searchTab') {
-  searchTabId = request.tabId;
-}*/
     if (request.action === 'isTab') {
         if (searchTabId.includes(sender.tab.id)) {
             sendResponse(1);
